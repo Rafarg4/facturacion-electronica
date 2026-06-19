bd5b5d14924210dd075a53b400d84bf3747c29c7
fredyverameza <fredy.vera@progresarcorp.com.py>
2026-06-19 12:31:02 -0300

    Menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,206 +1,272 @@
-<div class="input-group" data-widget="sidebar-search">
-<input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-<div class="input-group-append">
-<button class="btn btn-sidebar">
-<i class="fas fa-search fa-fw"></i>
-</button>
-</div>
-</div>
-<br>
 <li class="nav-item">
     <a href="{{ route('home') }}"
        class="nav-link {{ Request::is('home*') ? 'active' : '' }}">
-       <i class="nav-icon fas fa-tachometer-alt"></i>
+        <i class="nav-icon fas fa-tachometer-alt"></i>
         <p>Dashboard</p>
     </a>
 </li>
 
-<li class="nav-item">
-    <a href="{{ route('rubros.index') }}"
-       class="nav-link {{ Request::is('rubros*') ? 'active' : '' }}">
-      <i class="fa fa-clone" aria-hidden="true"></i>
-        <p>Rubros</p>
-    </a>
-</li>
-</li><li class="nav-item">
-    <a href="{{ route('cotizacions.index') }}"
-       class="nav-link {{ Request::is('cotizacions*') ? 'active' : '' }}">
-       <i class="fas fa-dollar-sign"></i>
-        <p>Cotizaciones</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('clientes.index') }}"
-       class="nav-link {{ Request::is('clientes*') ? 'active' : '' }}">
-       <i class="fa fa-users" aria-hidden="true"></i>
-        <p>Clientes</p>
-    </a>
-</li>
+<!-- INVENTARIO -->
 
-<li class="nav-item">
-    <a href="{{ route('presupuestoCabeceras.index') }}"
-       class="nav-link {{ Request::is('presupuestoCabeceras*') ? 'active' : '' }}">
-       <i class="fa fa-file" aria-hidden="true"></i>
-        <p>Presupuestos</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('cobros.index') }}"
-       class="nav-link {{ Request::is('cobros*') ? 'active' : '' }}">
-       <i class="fa fa-credit-card" aria-hidden="true"></i>
-        <p>Cobros</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('ventas.index') }}"
-       class="nav-link {{ Request::is('ventas*') ? 'active' : '' }}">
-       <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-        <p>Ventas</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('listaPrecios.index') }}"
-       class="nav-link {{ Request::is('listaPrecios*') ? 'active' : '' }}">
-       <i class="fa fa-tags" aria-hidden="true"></i>
-        <p>Lista Precios</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('productos.index') }}"
-       class="nav-link {{ Request::is('productos*') ? 'active' : '' }}">
-       <i class="fa fa-archive" aria-hidden="true"></i>
-        <p>Productos</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('proveedors.index') }}"
-       class="nav-link {{ Request::is('proveedors*') ? 'active' : '' }}">
-       <i class="fa fa-truck" aria-hidden="true"></i>
-        <p>Proveedors</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('compras.index') }}"
-       class="nav-link {{ Request::is('compras*') ? 'active' : '' }}">
-       <i class="fas fa-money-bill-wave"></i> <!-- carrito de compras -->
-        <p>Compras</p>
-    </a>
-</li>
+<li class="nav-item has-treeview">
 
-<li class="nav-item">
-    <a href="{{ route('pedidos.index') }}"
-       class="nav-link {{ Request::is('pedidos*') ? 'active' : '' }}">
-        <i class="fas fa-receipt"></i>
-        <p>Pedidos</p>
-    </a>
-</li>
 
-<li class="nav-item">
-    <a href=""
-       class="nav-link">
-        <i class="fas fa-file-invoice-dollar"></i>
-        <p>Reportes</p>
+<a href="#" class="nav-link">
+    <i class="nav-icon fas fa-boxes"></i>
+    <p>
+        Inventario
         <i class="right fas fa-angle-left"></i>
-    </a>
+    </p>
+</a>
 
-    <ul class="nav nav-treeview">
-
-        <li class="nav-item">
-            <a href="{{ route('ver_rendicion_caja') }}" class="nav-link">
-                <i class="fas fa-list"></i>
-                <p>Rendición de Caja</p>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a href="{{ route('ver_cobros_pendientes') }}" class="nav-link">
-                <i class="fas fa-list"></i>
-                <p>Cobros del Día</p>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a href="{{ route('ver_reporte_stock') }}" class="nav-link">
-                <i class="fas fa-boxes"></i>
-                <p>Reporte de Productos</p>
-            </a>
-        </li>
-
-    </ul>
-</li>
-<li class="nav-item">
-    <a href="{{ route('consulta_precio') }}" class="nav-link">
-        <i class="fas fa-barcode"></i>
-        <p>Consulta de Precios</p>
-    </a>
-</li>
-<li class="nav-item">
-
-    <a href="{{ route('auditoria') }}"
-       class="nav-link {{ Request::routeIs('auditoria') ? 'active' : '' }}">
-
-        <i class="nav-icon fas fa-history"></i>
-
-        <p>
-            Auditoría
-        </p>
-
-    </a>
-
-</li>
-<li class="nav-item">
-    <a href=""
-       class="nav-link ">
-       <i class="fas fa-cash-register"></i>
-        <p>Caja</p>
-        <i class="right fas fa-angle-left"></i>
-    </a>
 <ul class="nav nav-treeview">
-<li class="nav-item">
-    <a href="{{ route('cajas.index') }}"
-       class="nav-link {{ Request::is('cajas*') ? 'active' : '' }}">
-       <i class="fa fa-address-card" aria-hidden="true"></i>
-        <p>Cajas disponibles</p>
-    </a>
-</li>
-</li>
-<li class="nav-item">
-    <a href="{{ route('ver_cierres') }}"
-       class="nav-link {{ Request::is('ver_cierres*') ? 'active' : '' }}">
-      <i class="fa fa-history" aria-hidden="true"></i>
-        <p>Historial de Cierres </p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('cierre_caja') }}"
-       class="nav-link {{ Request::is('cierre_caja*') ? 'active' : '' }}">
-       <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
-        <p>Generar Cierre </p>
-    </a>
-</li>
+
+    <li class="nav-item">
+        <a href="{{ route('productos.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Productos</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('rubros.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Rubros</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('listaPrecios.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Lista de Precios</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('proveedors.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Proveedores</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('compras.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Compras</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('pedidos.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Pedidos</p>
+        </a>
+    </li>
+
 </ul>
+
+
 </li>
-<li class="nav-item">
-    <a href="{{ route('empresas.index') }}"
-       class="nav-link {{ Request::is('empresas*') ? 'active' : '' }}">
-       <i class="fa fa-building" aria-hidden="true"></i>
-        <p>Empresas</p>
-    </a>
+
+<!-- VENTAS -->
+
+<li class="nav-item has-treeview">
+
+
+<a href="#" class="nav-link">
+    <i class="nav-icon fas fa-shopping-cart"></i>
+    <p>
+        Ventas
+        <i class="right fas fa-angle-left"></i>
+    </p>
+</a>
+
+<ul class="nav nav-treeview">
+
+    <li class="nav-item">
+        <a href="{{ route('clientes.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Clientes</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('presupuestoCabeceras.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Presupuestos</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('ventas.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Ventas</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('cobros.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Cobros</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('consulta_precio') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Consulta de Precios</p>
+        </a>
+    </li>
+
+</ul>
+
+
 </li>
-<li class="nav-item">
-    <a href="{{ route('users.index') }}"
-       class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
-       <i class="fa fa-light fa-user"></i>
-        <p>Usuarios</p>
-    </a>
+
+<!-- CAJA -->
+
+<li class="nav-item has-treeview">
+
+
+<a href="#" class="nav-link">
+    <i class="nav-icon fas fa-cash-register"></i>
+    <p>
+        Caja
+        <i class="right fas fa-angle-left"></i>
+    </p>
+</a>
+
+<ul class="nav nav-treeview">
+
+    <li class="nav-item">
+        <a href="{{ route('cajas.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Cajas Disponibles</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('ver_cierres') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Historial de Cierres</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('cierre_caja') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Generar Cierre</p>
+        </a>
+    </li>
+
+</ul>
+
+
 </li>
+
+<!-- REPORTES -->
+
+<li class="nav-item has-treeview">
+
+
+<a href="#" class="nav-link">
+    <i class="nav-icon fas fa-chart-bar"></i>
+    <p>
+        Reportes
+        <i class="right fas fa-angle-left"></i>
+    </p>
+</a>
+
+<ul class="nav nav-treeview">
+
+    <li class="nav-item">
+        <a href="{{ route('ver_rendicion_caja') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Rendición de Caja</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('ver_cobros_pendientes') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Cobros del Día</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('ver_reporte_stock') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Reporte de Productos</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('auditoria') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Auditoría</p>
+        </a>
+    </li>
+
+</ul>
+
+
+</li>
+
+<!-- CONFIGURACION -->
+
+<li class="nav-item has-treeview">
+
+
+<a href="#" class="nav-link">
+    <i class="nav-icon fas fa-cogs"></i>
+    <p>
+        Configuración
+        <i class="right fas fa-angle-left"></i>
+    </p>
+</a>
+
+<ul class="nav nav-treeview">
+
+    <li class="nav-item">
+        <a href="{{ route('empresas.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Empresas</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('users.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Usuarios</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('cotizacions.index') }}" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Cotizaciones</p>
+        </a>
+    </li>
+
+</ul>
+
+
+</li>
+
+<!-- SALIR -->
+
 <li class="nav-item">
-    <a href="" 
-       class="nav-link " onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-      <i class="fas fa-sign-out-alt"></i>
-        <p>Salir</p>
-    </a>
 
 
+<a href=""
+   class="nav-link"
+   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+
+    <i class="fas fa-sign-out-alt"></i>
+
+    <p>Salir</p>
+
+</a>
 
 
+</li>
